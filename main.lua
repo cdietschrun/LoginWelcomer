74bbf835a38e2330480e5379a267129a9ec08ab6
cdietschrun <cdietschrunfast@gmail.com>
2018-07-03 16:11:51 -0400

    Add toc file and simplify string

--- main.lua
+++ main.lua
@@ -1,9 +1,4 @@
 local currentTitleId = GetCurrentTitle()
 local titleName = GetTitleName(currentTitleId)
 
-local firstChar = string.sub(titleName, 1, 1)
-if firstChar == ' ' then
-	print("Welcome, " .. UnitName("player") .. ", " .. titleName)
-else
-	print ("Welcome, " .. UnitName("player") .. " " ..  titleName)
-end
+print ("Welcome " .. UnitName("player") .. ", your title is: " .. titleName)
